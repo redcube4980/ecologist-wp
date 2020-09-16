1e8a6d71cddc7234869643cbdf8fc8903c0fd887
yamakawa <yamakawa@yamakawa-Mac-Pro.local>
2020-09-16 13:54:54 +0900

    2020/09/16/13:54

--- page-overview.php
+++ page-overview.php
@@ -19,7 +19,7 @@ Template Name: overview
 	</div>
 </div>
 </div>
-<<?php if (have_posts()): ?>
+<?php if (have_posts()): ?>
 		<?php while (have_posts()) : the_post(); ?>
 		<?php the_content(); ?>
 		<?php endwhile; ?>
